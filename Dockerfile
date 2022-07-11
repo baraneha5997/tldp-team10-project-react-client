6e85066d6be7a65ed1500617b7662458dbebc9be
William Immendorf <1304819+WilliamImm@users.noreply.github.com>
2022-07-11 09:58:41 -0500

    use kube service names

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ RUN npm run build
 
 FROM nginx:stable-alpine
 COPY --from=build /app/build/ /usr/share/nginx/html
-COPY --from=build /app/default.conf /etc/nginx/http.d/default.conf
+COPY --from=build /app/default.conf /etc/nginx/templates/default.conf.template
 EXPOSE 80
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require('http-proxy-middleware');
+const process = require('process');
 
 module.exports = function(app) {
-    app.use(proxy('/api', { target: 'http://localhost:8000/' }));
+    app.use(proxy('/api', { target: `http://${process.env.PROJECT_SERVER_SERVICE_HOST}:${process.env.PROJECT_SERVER_SERVICE_PORT}/` }));
 };
